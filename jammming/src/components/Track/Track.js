9158ace5b2faea57398f6fae4b7910d22a2ec51a
Evan Galt <evan.galt@gmail.com>
2023-05-26 09:51:06 -0500

    set up file structure with components

---
+++ jammming/src/components/Track/Track.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Track() {
+    return (
+        // @todo
+    );
+};
+
+export default Track;
---
+++ jammming/src/components/Tracklist/Tracklist.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Tracklist() {
+    return (
+        // @todo
+    );
+};
+
+export default Tracklist;
